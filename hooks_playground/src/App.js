5e2a6e1ad831b40642dbe8c788638ad2c323cb1e
“Wes <wesleyrwhite86@gmail.com>
2019-08-03 18:36:58 -0700

    Review of stateless components

--- hooks_playground/src/App.js
+++ hooks_playground/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
-function App() {
+
+function Box(props) {
+  //this is the child rendered by parent
   return (
     <div className="App">
       <h1>React Hooks Playground</h1>
+      <p>{props.text}</p>
+    </div>
+  );
+}
+
+function App() {
+  //this is the parent
+  return (
+    <div>
+      <Box text="Hi, Wesley from app props." />;
     </div>
   );
 }
